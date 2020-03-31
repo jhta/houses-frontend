39940db0a60b1e06aaedad23c49b113ea44f187b
ivgomez <iv.gomez@hotmail.com>
2020-03-30 22:14:04 -0500

    add page map

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -7,8 +7,13 @@ export const Header = () => (
     <Container>
       <Row className="justify-between">
         <Column>
-          <div className="py-4">
+          <div className="py-4 flex justify-center items-center text-center">
             <img className="h-6" src="/images/logo.svg" />
+            <a className="text-gray-2 px-4 text-lg cursor-pointer" href="/map/mapPage">
+              <div className="flex text-secondary mx-2 w-20 justify-center items-center bg-fourth rounded-full h-8">
+                Buscar
+              </div>
+            </a>
           </div>
         </Column>
         <HeaderLinks></HeaderLinks>
---
+++ config/confg.js
@@ -0,0 +1,3 @@
+export const getKey = {
+  MPBX_key: 'pk.eyJ1IjoiaXZhbmdhcmNpYSIsImEiOiJjazhkZnk1cGgwdHM3M2Z0N2J4aWRoN3RvIn0.lFVKfgm9XJc9FYqAoXfB-g',
+};
